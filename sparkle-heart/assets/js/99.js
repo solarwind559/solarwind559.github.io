3c28d661309d5418f4ac0bd940b3e1eb6d9ba349
solarwind559 <74195040+solarwind559@users.noreply.github.com>
2021-03-25 15:39:48 +0100

    Delete sparkle-heart directory

--- sparkle-heart/assets/js/99.js
+++
@@ -1,7 +0,0 @@
-const more = document.getElementById("more");
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-more.style.display = 'block';
-button.style.display = 'none';
-});
