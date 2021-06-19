c8a08b566fdb7a9815ae7069cd80561e052573a1
solarwind559 <74195040+solarwind559@users.noreply.github.com>
2021-06-19 12:01:52 +0200

    Delete Task_2_Sanda_Polna directory

--- Task_2_Sanda_Polna/assets/js/mini-menu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var toggle = document.getElementById("myLinks");
-  if (toggle.style.display === "block") {
-    toggle.style.display = "none";
-  } else {
-    toggle.style.display = "block";
-  }
-}
